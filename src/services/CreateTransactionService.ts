638b90232443a3911981fdc78c750451edbb7a5b
BLoboF <brunofigueiredo_3@hotmail.com>
2020-07-17 11:16:25 -0300

    services-update-new-rule-transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -15,6 +15,10 @@ class CreateTransactionService {
   }
 
   public execute({ title, value, type }: Request): Transaction {
+    if(!['income', 'outcome'].includes(type)){
+      throw new Error(`Transaction Invalid. Choose 'income' or 'outcome'.`);
+    }
+
     const balance = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && value > balance.total) {
